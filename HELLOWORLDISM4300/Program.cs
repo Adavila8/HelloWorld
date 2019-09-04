51e8e3c830b2c1e2d99f2c4d212ec2ae1c202157
Adavila8 <aarondavila@mail.usf.edu>
2019-09-03 20:07:04 -0400

    First Commit

---
+++ HELLOWORLDISM4300/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HELLOWORLDISM4300
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to continue...");
+            Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
+        }
+    }
+}
